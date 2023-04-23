abe874795827b6fb3e027db403197bb1bcebf4fa
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-23 10:38:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+
+
+const majorityElement = (arr = []) => {
+   const threshold = Math.floor(arr.length / 2);
+   const map = {};
+   for (let i = 0; i < arr.length; i++) {
+      const value = arr[i];
+      map[value] = map[value] + 1 || 1;
+      if (map[value] > threshold)
+         return value
+   };
+   return false;
+};
+console.log(majorityElement(arr));
